25e007577bc376c2d072aea19381fd9222582209
liuyikai152 <liuyknet.163.com>
2019-01-03 19:58:20 +0800

    ytq

---
+++ SmartFactory/SmartFactory.Model/Business.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFactory.Model
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
+    /// <summary>
+    /// 设备业务表
+    /// </summary>
+    [Table("Business")]
+    public  class Business
+    {
+        /// <summary>
+        /// 主键id
+        /// </summary>
+        [Key]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 业务名称
+        /// </summary>
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 业务编号
+        /// </summary>
+        [MaxLength(20)]
+        public string Number { get; set; }
+    }
+}
