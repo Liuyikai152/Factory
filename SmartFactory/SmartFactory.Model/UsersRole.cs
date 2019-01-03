528267de97534da51351583c66ee4fdf1887083f
liuyikai152 <liuyknet@163.com>
2019-01-03 20:49:21 +0800

    fk

---
+++ SmartFactory/SmartFactory.Model/UsersRole.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFactory.Model
+{
+
+    /// <summary>
+    /// 用户角色关系表
+    /// </summary>
+   public class UsersRole
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 用户id
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// 角色id
+        /// </summary>
+        public int RoleId { get; set; }
+    }
+}
