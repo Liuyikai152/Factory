aa7496dc8a9839801181bb71a56a386ef09321ba
liuyikai152 <liuyknet.163.com>
2019-01-04 13:33:58 +0800

    ytq

--- SmartFactory/SmartFactory.Model/FacilityLoca.cs
+++ SmartFactory/SmartFactory.Model/FacilityLoca.cs
@@ -19,14 +19,14 @@ namespace SmartFactory.Model
         /// 主键id
         /// </summary>
         [Key]
-        public int ID { get; set; }
+        public int ID { get; set; }          
 
         /// <summary>
         /// 设备位号
         /// </summary>
         [MaxLength(20)]
         public string LocaName { get; set; }
-      
+        
         /// <summary>
         /// 设备位号
         /// </summary>
