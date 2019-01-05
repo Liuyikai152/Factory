08bb652d6648f7879226b68e4d1a51d93db2f64d
MoLingMoMing <45552951+MoLingMoMing@users.noreply.github.com>
2019-01-05 09:16:33 +0800

    kyy

---
+++ SmartFactory/SmartFactory.Api/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using SmartFactory.IServices;
+using SmartFactory.Model;
+using SmartFactory.Services;
+
+namespace SmartFactory.Api.Controllers
+{
+    [RoutePrefix("Users")]
+    public class UsersController : ApiController
+    {
+
+        public IUsers userservices { get; set; }
+
+        [Route("UsersAdd")]
+        [HttpPost]
+        public int UsersAdd(Users user)
+        {
+            int i= userservices.Add(user);
+            return i;
+        }
+    }
+}
--- SmartFactory/SmartFactory.IServices/IUsers.cs
+++ SmartFactory/SmartFactory.IServices/IUsers.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SmartFactory.Model;
+
 namespace SmartFactory.IServices
 {
-    public class Class1
+    public interface IUsers
     {
+        int Add(Users user);
     }
 }
