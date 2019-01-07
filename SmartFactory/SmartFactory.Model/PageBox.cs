05273bab25f0c4eba7b8a6ddd92ebd952bea321a
Liuyikai152 <liuyknet@163.com>
2019-01-07 09:46:34 +0800

    222

---
+++ SmartFactory/SmartFactory.Model/PageBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+    public class PageBox
+    {
+        /// <summary>
+        /// 当前页
+        /// </summary>
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// 总页数
+        /// </summary>
+        public int PageCount { get; set; }
+
+        /// <summary>
+        /// 每页几条数据
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 显示数据
+        /// </summary>
+        public object Data{ get; set; }
+    }
