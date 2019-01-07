36c1087c1ae208e6f7afb44ee48ffca52be7ebe0
Liuyikai152 <liuyknet@163.com>
2019-01-07 18:43:12 +0800

    111

--- SmartFactory/SmartFactory.Api/Controllers/FacilityController.cs
+++ SmartFactory/SmartFactory.Api/Controllers/FacilityController.cs
@@ -8,23 +8,30 @@ using System.Web.Http;
 using SmartFactory.IServices;
 using SmartFactory.Model;
 using SmartFactory.Services;
+using Newtonsoft.Json;
 namespace SmartFactory.Api.Controllers
 {
     [RoutePrefix("Facility")]
     public class FacilityController : ApiController
     {
         public IFacilityServices FacilityServices { get; set; }
+
+        private const int PAGESIZE = 4;
         /// <summary>
         /// 查看所有零件
         /// </summary>
         /// <returns></returns>
         [HttpGet]
         [Route("GetFacility")]
-        public List<Facility> GetFacility()
+        public string GetFacility(int  Page=1)
         {
-
-            var getFacility = FacilityServices.GetFacility();
-            return getFacility;
+            List<Facility> facilitiesList = FacilityServices.GetFacility();
+            PageBox pagebox = new PageBox();
+            pagebox.PageIndex = Page;
+            pagebox.PageCount = facilitiesList.Count / PAGESIZE + (facilitiesList.Count % PAGESIZE == 0 ? 0 : 1);
+            pagebox.Data = facilitiesList.Skip((Page - 1) * PAGESIZE).Take(PAGESIZE);
+            var json = JsonConvert.SerializeObject(pagebox);
+            return json;
         }
     }
 }
