15f18e7ed1f43b0540835ff4c2a4b902a079acb7
Liuyikai152 <liuyknet@163.com>
2019-01-08 15:47:54 +0800

    Update FacilityServices.cs

--- SmartFactory/SmartFactory.Services/FacilityServices.cs
+++ SmartFactory/SmartFactory.Services/FacilityServices.cs
@@ -29,18 +29,14 @@ namespace SmartFactory.Services
         }
 
 
+
         /// <summary>
         /// 查看设备/零件数据
         /// </summary>
         /// <returns></returns>
-        public List<FacilityNotMapp> GetFacility()
+        public List<Facility> GetFacility()
         {
-            var acilityList = factoryDBcontext.Database.SqlQuery<FacilityNotMapp>("CALL pro_getFacilitys").ToList();
-            return acilityList;
-        }
 
-       
-         
             var result = factoryDBcontext.Facility.ToList();
             return result;
             //var getFacility = from a in factoryDBcontext.Facility.ToList()
