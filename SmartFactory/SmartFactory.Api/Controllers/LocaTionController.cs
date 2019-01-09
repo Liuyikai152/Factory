09bcd055112b1e79a0f8d44883adfcda86a8809f
liuyikai152 <liuyknet.163.com>
2019-01-09 09:40:27 +0800

    ytq
    q

--- SmartFactory/SmartFactory.Api/Controllers/LocaTionController.cs
+++ SmartFactory/SmartFactory.Api/Controllers/LocaTionController.cs
@@ -18,21 +18,35 @@ namespace SmartFactory.Api.Controllers
     {
         public ILocalionServices localionServices { get; set; }
 
+        
         /// <summary>
         /// 查询功能位置信息
         /// </summary>
         /// <returns></returns>
         [HttpGet]
+        [HttpPost]
         [Route("GetLocaTions")]
-        public PageBox GetLocaTions(int PAGESIZE=5,int page=1)
+        public PageBox GetLocaTions(string currentpage,string companyname)
         {
-            List<LocationNotMapped> list = localionServices.GetLocations();
-            PageBox pageBox = new PageBox();
-            pageBox.PageIndex = page;
-            pageBox.PageSize = PAGESIZE;
-            pageBox.PageCount=list.Count / PAGESIZE + (list.Count % PAGESIZE == 0 ? 0 : 1);
-            pageBox.Data= list.Skip((page - 1) * PAGESIZE).Take(PAGESIZE);
-            return pageBox;
+            var locaList = localionServices.GetLocations();
+            if (companyname!=null)
+            {
+                locaList = locaList.Where(n => n.CompanyName.Contains(companyname)).ToList();
+            }
+
+            if (currentpage == null)
+            {
+                currentpage = "1";
+            }
+            
+            int totlepage = locaList.Count / 3 + (locaList.Count % 3 == 0 ? 0 : 1);
+            locaList = locaList.Skip((int.Parse(currentpage) - 1) * 3).Take(3).ToList();
+            PageBox pagebox = new PageBox();
+            pagebox.PageIndex = int.Parse(currentpage);
+            pagebox.PageCount = totlepage;
+            pagebox.Data = locaList;
+
+            return  pagebox;
         }
 
         /// <summary>
