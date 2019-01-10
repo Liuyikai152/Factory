fcca5816ae2ca926e6634548f7e27e878ff4af30
MoLingMoMing <45552951+MoLingMoMing@users.noreply.github.com>
2019-01-10 09:30:16 +0800

    kyy

--- SmartFactory/SmartFactory.Api/Controllers/ExcelController.cs
+++ SmartFactory/SmartFactory.Api/Controllers/ExcelController.cs
@@ -16,15 +16,24 @@ using SmartFactory.Model;
 using System.Configuration;
 using System.Web;
 
+using SmartFactory.Services;
+using SmartFactory.IServices;
+using SmartFactory.Model;
+using Newtonsoft.Json;
 
 namespace SmartFactory.Api.Controllers
 {
+    [RoutePrefix("Excel")]
     public class ExcelController : ApiController
     {
-
+        #region EXCEL导入
+        /// <summary>
+        /// 维修工单导入
+        /// </summary>
+        /// <returns></returns>
         [HttpPost]
-        [Route("Imports")]
-        public int Imports()
+        [Route("ImportsExcel")]
+        public int ImportsExcel()
         {
             FactoryDBcontext dBcontext = new FactoryDBcontext();
             //获取所选文件
@@ -36,7 +45,7 @@ namespace SmartFactory.Api.Controllers
                 if (!System.IO.File.Exists(fileName))
                     getFile.SaveAs(fileName);
                 //把Excel当做数据源连接
-                string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=Excel 12.0;";
+                string connStr = GetConnectionString(fileName);
 
                 //打开Excel
                 OleDbConnection conn = new OleDbConnection(connStr);
@@ -54,20 +63,20 @@ namespace SmartFactory.Api.Controllers
                 DataTable dt = ds.Tables[0];
                 Facility facility = new Facility();
 
-                using (MySqlConnection mySqlConnection = new MySqlConnection(ConfigurationManager.ConnectionStrings["connString"].ConnectionString))
+                using (MySqlConnection mySqlConnection = new MySqlConnection(ConfigurationManager.ConnectionStrings["ArtLineDbContext"].ConnectionString))
                 {
                     MySqlCommand mySqlCommand = new MySqlCommand();
                     mySqlCommand.Connection = mySqlConnection;
+                    mySqlConnection.Open();
                     for (int i = 0; i < dt.Rows.Count; i++)
                     {
                         //获得添加的sql语句 并执行
-
-                        string stuSQL = string.Format("insert into facility(FacilityNumber,FacilityName,BitNumber, CompanyCode,PlanFactory,MaintainFactory,FactoryArea,PlanGroup,CostConter,FacilityType,FacilityLoca,StartDate,DateChanged,BusinessLine,IsInstall,IsDelete,ServiceNumber,IsSiren) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}','{13}','{14}','{15}','{16}','{17}')", dt.Rows[i]["设备编号"], dt.Rows[i]["设备名称"], dt.Rows[i]["设备位号"], dt.Rows[i]["公司编号"], dt.Rows[i]["计划工厂"], dt.Rows[i]["维护工厂"], dt.Rows[i]["工厂区域"], dt.Rows[i]["计划员组"], dt.Rows[i]["成本中心"], dt.Rows[i]["设备种类"], dt.Rows[i]["设备安装位置"], dt.Rows[i]["开始日期"], dt.Rows[i]["修改日期"], dt.Rows[i]["业务范围"], dt.Rows[i]["是否安装位置"], dt.Rows[i]["包含删除"], dt.Rows[i]["维护班组编号"], dt.Rows[i]["是否报警"]);
+                        string stuSQL = string.Format("insert into MaintenanceOrder(OrderNumber,Belongs,DeviceNumber,DeviceName,FaultName,FaultPart,HappenTime,CompletionTime,DownTime,HandlingMethod) values('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}')",dt.Rows[i]["通知单号"],dt.Rows[i]["所属机组/机泵"],dt.Rows[i]["设备编号"],dt.Rows[i]["设备名称"],dt.Rows[i]["故障名称"],dt.Rows[i]["故障部位"],dt.Rows[i]["故障发生时间"],dt.Rows[i]["处理完成时间"],dt.Rows[i]["停机时间"],dt.Rows[i]["故障处理方法"]);
                         mySqlCommand.CommandText = stuSQL;
                         var addpowers = mySqlCommand.ExecuteNonQuery();
 
                     }
-
+                    mySqlConnection.Close();
                 }
                 conn.Close();
 
@@ -77,6 +86,281 @@ namespace SmartFactory.Api.Controllers
             {
                 return 0;
             }
+
+        }
+        #endregion
+
+        #region 导出到EXCEL
+
+
+        /// <summary>
+        /// 将数据导出到指定的Excel文件中
+        /// </summary>
+        /// <param name="listView">System.Windows.Forms.ListView,指定要导出的数据源</param>
+        /// <param name="destFileName">指定目标文件路径</param>
+        /// <param name="tableName">要导出到的表名称</param>
+        /// <param name="overWrite">指定是否覆盖已存在的表</param>
+        /// <returns>导出的记录的行数</returns>
+        [HttpPost]
+        [Route("ExportToExcel")]
+        public static int ExportToExcel(System.Data.DataTable dt, string destFileName, string tableName)
+
+        {
+
+            if (File.Exists(destFileName)) 
+            {
+                File.Delete(destFileName);
+            }
+
+            //得到字段名
+
+            string szFields = "";
+
+            string szValues = "";
+
+            for (int i = 0; i < dt.Columns.Count; i++)
+
+            {
+
+                szFields += "[" + dt.Columns[i] + "],";
+
+            }
+
+            szFields = szFields.TrimEnd(',');
+
+            //定义数据连接
+
+            OleDbConnection connection = new OleDbConnection();
+
+            connection.ConnectionString = GetConnectionString(destFileName);
+
+            OleDbCommand command = new OleDbCommand();
+
+            command.Connection = connection;
+
+            command.CommandType = CommandType.Text;
+
+            //打开数据库连接
+
+            try
+
+            {
+
+                connection.Open();
+
+            }
+
+            catch
+
+            {
+
+                throw new Exception("目标文件路径错误。");
+
+            }
+
+            //创建数据库表
+
+            try
+
+            {
+                command.CommandText = GetCreateTableSql("[" + tableName + "]", szFields.Split(','));
+                command.ExecuteNonQuery();
+            }
+
+            catch (Exception ex)
+
+            {
+
+                //如果允许覆盖则删除已有数据
+
+                throw ex;
+
+            }
+
+            try
+
+            {
+
+                //循环处理数据------------------------------------------
+
+                int recordCount = 0;
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+
+                {
+
+                    szValues = "";
+
+                    for (int j = 0; j < dt.Columns.Count; j++)
+
+                    {
+
+                            szValues += "'" + dt.Rows[i][j] + "',";
+
+                        
+
+                    }
+
+                    szValues = szValues.TrimEnd(',');
+
+                    //组合成SQL语句并执行
+
+                    string szSql = "INSERT INTO [" + tableName + "](" + szFields + ") VALUES(" + szValues + ")";
+
+                    command.CommandText = szSql;
+
+                    recordCount += command.ExecuteNonQuery();
+
+                }
+
+                connection.Close();
+
+                return recordCount;
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                throw ex;
+
+            }
+
+        }
+
+        //得到连接字符串
+
+        private static String GetConnectionString(string fullPath)
+
+        {
+
+            string szConnection;
+
+            szConnection = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fullPath + ";Extended Properties=Excel 12.0;";
+
+            return szConnection;
+
+        }
+
+ 
+
+ 
+
+        //得到创建表的SQL语句
+
+        private static string GetCreateTableSql(string tableName, string[] fields)
+
+        {
+
+            string szSql = "CREATE TABLE " + tableName + "(";
+
+            for (int i = 0; i < fields.Length; i++)
+
+            {
+
+                szSql += fields[i] + " VARCHAR(200),";
+
+            }
+
+            szSql = szSql.TrimEnd(',') + ")";
+
+            return szSql;
+
+        }
+
+
+
+
+
+        #endregion
+
+        [Route("OutExcel")]
+        [HttpPost]
+        public  string OutExcel()
+        {
+
+            DataTable dt = GetDataTable();
+
+
+
+            string FileName = Guid.NewGuid().ToString() + ".xls";
+
+            string sNewFullFile = HttpContext.Current.Request.MapPath(FileName);
+            try
+            {
+                File.Copy(HttpContext.Current.Request.MapPath("format.xls"), sNewFullFile);
+            }
+            catch (Exception er)
+            {
+                return er.Message;
+                
+            }
+
+            string strConn = GetConnectionString(sNewFullFile);
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection(strConn);
+            OleDbCommand cmd = null;
+
+            bool bRet = false;
+            try
+            {
+                conn.Open();
+                cmd = new OleDbCommand("create table [sheet1]([通知单号] Text,[所属机组/机泵] Text,[设备编号] Text,[设备名称] Text,[故障名称] Text,[故障部位] Text,[故障发生时间] Text,[处理完成时间] Text,[停机时间] Text,[故障处理方法] Text)", conn);
+                cmd.ExecuteNonQuery();
+
+
+
+                string strSQL = "INSERT INTO [Sheet1$] ([通知单号], [所属机组/机泵],[设备编号],[设备名称],[故障名称],[故障部位],[故障发生时间],[处理完成时间],[停机时间],[故障处理方法]) VALUES (?, ?, ?)";
+
+                cmd = new OleDbCommand(strSQL, conn);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    cmd.Parameters.Add(i.ToString(), OleDbType.VarChar);
+                }
+
+
+
+                DataView dv = dt.DefaultView;
+                foreach (DataRowView row in dv)
+                {
+
+                    cmd.Parameters[0].Value = row["OrderNumber"].ToString();
+                    cmd.Parameters[1].Value = (int)row["age"];
+                    cmd.Parameters[2].Value = row["phone"].ToString();
+                    cmd.ExecuteNonQuery();
+                }
+                bRet = true;
+
+
+            }
+            catch (Exception er)
+            {
+                 return er.Message;
+            }
+            finally
+            {
+                if (cmd != null)
+                {
+                    cmd.Dispose();
+                }
+                conn.Dispose();
+
+            }
+            if (bRet)
+                return  FileName;
+
+
+
+            return FileName;
+        }
+
+        public IMaintenanceOrderServices maintenanceOrderServices { get; set; }
+
+        public DataTable GetDataTable() {
+            var list = maintenanceOrderServices.GetMaintenanceOrders(null);
+
+            return JsonConvert.DeserializeObject<DataTable>(JsonConvert.SerializeObject(list));
 
         }
 
