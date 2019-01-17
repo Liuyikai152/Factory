0ae14d168f5a8af97131241b1822c5fcf5a657d5
liuyikai152 <liuyknet.163.com>
2019-01-17 10:27:21 +0800

    ytq

---
+++ SmartFactory/SmartFactory.Common/DapperHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartFactory.Common
+{
+    using System.Configuration;
+    using Dapper;
+    using MySql.Data.MySqlClient;
+    using MySql.Data;
+
+    public class DapperHelper
+    {
+        public static string connString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+
+        /// <summary>
+        /// 获取连接数据库的字符串
+        /// </summary>
+        /// <returns></returns>
+        public static MySqlConnection GetConnString()
+        {
+            return new MySqlConnection(connString);
+        }
+    }
+}
